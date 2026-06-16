61d03c2d7a12380ed58cb41fcb6a88297c319970
MarcZxc1 <dagodemarcgeraldarante@gmail.com>
2026-06-16 15:41:52 +0800

    chore: remove temporary meow directory

--- meow/src/routes/user.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { createUser, listUsers } from "../controllers/user.controller";
-
-export const userRouter = Router();
-
-userRouter.get("/", listUsers);
-userRouter.post("/", createUser);
